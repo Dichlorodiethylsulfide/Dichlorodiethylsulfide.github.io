6181a5f4d3ea6ebe9f8e133d6f3e8ad06346425e
Dichlorodiethylsulfide <rikilowe8@gmail.com>
2021-04-28 04:22:48 +0100

    Changed index.html to generate based on site data. Original backed up.

--- js/config.js
+++ js/config.js
@@ -1,14 +1,127 @@
 export default
 {
-  "AA":"Automated Abyss",
-  "PB":"Polybius",
-  "SI":"Space Invaders",
-  "PH":"Perlin Horror",
-  "RIP":"Reflective In Perspective",
-  "AICPG":"AI Controller (Procedural Generation)",
-  "PM":"Password Manager",
-  "YAGE":"YetAnotherGameEngine",
-  "VMPH":"Virtual Machine Python Handler",
-  "AG":"Artstation Gallery",
-  "SH":"Shopping Helper"
+  "AA":
+  {
+    "Abbreviation":"AA",
+    "Title":"Automated Abyss",
+    "Genres":["3D", "Strategy", "Tower Defense"],
+    "Category":"Games",
+    "Description":"Based on the operations and functions of a computer, this game uses Assembly x86 style commands to control buildings and towers aimed at defending the main base, the pulsing Clock, from invading attackers.<br><br>Includes a custom scripting langauge for user-generated tutorials, cutscenes and scenarios.",
+    "Thumbnail":"",
+    "Gallery":[],
+    "External Links":
+    {
+        "Itch.io":"https://dichlorodiethylsulfide.itch.io/automated-abyss"
+    },
+  },
+  "PB":
+  {
+    "Abbreviation":"PB",
+    "Title":"Polybius",
+    "Genres":["2D", "Strategy", "Tower Defense"],
+    "Category":"Games",
+    "Description":"",
+    "Thumbnail":"",
+    "Gallery":[],
+    "External Links":{},
+  },
+  "SI":
+  {
+    "Abbreviation":"SI",
+    "Title":"Space Invaders",
+    "Genres":["2D", "Strategy", "Bullet Hell"],
+    "Category":"Games",
+    "Description":"",
+    "Thumbnail":"",
+    "Gallery":[],
+    "External Links":{},
+  },
+  "PH":
+  {
+    "Abbreviation":"PH",
+    "Title":"Perlin Horror",
+    "Genres":["3D", "Randomly Generated", "Horror", "Psychological Experience"],
+    "Category":"Games",
+    "Description":"",
+    "Thumbnail":"",
+    "Gallery":[],
+    "External Links":{},
+  },
+  "RIP":
+  {
+    "Abbreviation":"RIP",
+    "Title":"Reflective In Perspective",
+    "Genres":["2D", "Top-down", "Thriller", "Slasher"],
+    "Category":"Visual Novel",
+    "Description":"",
+    "Thumbnail":"",
+    "Gallery":[],
+    "External Links":{},
+  },
+  "AICPG":
+  {
+    "Abbreviation":"AICPG",
+    "Title":"AI Controller / Procedural Generation",
+    "Genres":["3D", "Randomly Generated", "Terrain", "Tool", "AI"],
+    "Category":"Application",
+    "Description":"",
+    "Thumbnail":"",
+    "Gallery":[],
+    "External Links":{},
+  },
+  "PM":
+  {
+    "Abbreviation":"PM",
+    "Title":"Password Manager",
+    "Genres":["Console", "Tool", "Passwords"],
+    "Category":"Application",
+    "Description":"",
+    "Thumbnail":"",
+    "Gallery":[],
+    "External Links":{},
+  },
+  "YAGE":
+  {
+    "Abbreviation":"YAGE",
+    "Title":"YetAnotherGameEngine",
+    "Genres":["2D", "Game Engine", "SFML"],
+    "Category":"Application",
+    "Description":"",
+    "Thumbnail":"",
+    "Gallery":[],
+    "External Links":{},
+  },
+  "VMPH":
+  {
+    "Abbreviation":"VMPH",
+    "Title":"Virtual Machine Python Handler",
+    "Genres":["Virtual Machine", "Python", "Client-Server"],
+    "Category":"Application",
+    "Description":"",
+    "Thumbnail":"",
+    "Gallery":[],
+    "External Links":{},
+  },
+  "AG":
+  {
+    "Abbreviation":"AG",
+    "Title":"Artstation Gallery",
+    "Genres":["Artwork", "Gallery", "Showcase", "2D", "3D"],
+    "Category":"Artwork",
+    "Description":"",
+    "Thumbnail":"",
+    "Gallery":[],
+    "External Links":{},
+  },
+  "SH":
+  {
+    "Abbreviation":"SH",
+    "Title":"Shopping Helper",
+    "Genres":["Mobile App", "Android", "iOS", "Shopping", "Assistant"],
+    "Category":"Substantial Piece",
+    "Description":"",
+    "Thumbnail":"",
+    "Gallery":[],
+    "External Links":{},
+  }
 }
--- js/website.js
+++ js/website.js
@@ -9,16 +9,140 @@ var CurrentDataSet;
 
 function Startup()
 {
-  if(window.location.href.includes("template"))
+  CommitDataToStorage("SiteData", JSON.stringify(configData));
+  CurrentDataSet = JSON.parse(GetDataFromStorage("SiteData"))["default"];
+  if(window.location.href.includes("Template"))
   {
     TemplateStartup();
   }
   else {
-    StartupButtons();
+    MainPageStartup();
   }
 }
 
-function StartupButtons()
+function GetAllDataWhichIs(criteria)
+{
+  var fulfilled = {};
+  for(var k in CurrentDataSet)
+  {
+    var data = CurrentDataSet[k][criteria];
+    if(!(k in fulfilled))
+    {
+      fulfilled[k] = data;
+    }
+  }
+  return fulfilled;
+}
+
+function GetAllUniqueData(criteria)
+{
+  var uniqueData = [];
+  for(var k in CurrentDataSet)
+  {
+    var data = CurrentDataSet[k][criteria];
+    if(!uniqueData.includes(data))
+    {
+      uniqueData.push(data);
+    }
+  }
+  return uniqueData;
+}
+
+function CreateCategories()
+{
+  var projectList = document.getElementById("Projects");
+  var projectData = GetAllUniqueData("Category");
+  for(var category in projectData)
+  {
+    var aNode = document.createElement("A");
+    aNode.setAttribute("href", "#" + projectData[category]);
+    aNode.innerHTML = projectData[category];
+    projectList.appendChild(aNode);
+    var tabDiv = document.createElement("DIV");
+    tabDiv.setAttribute("class", "tab");
+    for(var data in CurrentDataSet)
+    {
+      if(CurrentDataSet[data]["Category"] == projectData[category])
+      {
+        var aaNode = document.createElement("A");
+        aaNode.setAttribute("href", "#" + CurrentDataSet[data]["Abbreviation"]);
+        aaNode.innerHTML = CurrentDataSet[data]["Title"];
+        aaNode.appendChild(document.createElement("BR"));
+        tabDiv.appendChild(aaNode);
+      }
+    }
+    projectList.appendChild(tabDiv);
+  }
+}
+
+function CreateSummaries()
+{
+  var summary = document.getElementById("ProjectSummaries");
+  var projectCategories = GetAllUniqueData("Category");
+  for(var category in projectCategories)
+  {
+    var h3title = document.createElement("H3");
+    h3title.setAttribute("id", projectCategories[category])
+    h3title.setAttribute("class", "BackgroundText");
+    h3title.innerHTML = projectCategories[category];
+    document.body.appendChild(h3title);
+    var categoryBox = CreateMainBox(document.body);
+    for(var data in CurrentDataSet)
+    {
+      if(CurrentDataSet[data]["Category"] == projectCategories[category])
+      {
+        CreateBox(categoryBox, CurrentDataSet[data]);
+      }
+    }
+  }
+}
+
+function CreateMainBox(appendTo)
+{
+  var mainBox = document.createElement("DIV");
+  mainBox.setAttribute("class", "MainBox");
+  appendTo.appendChild(mainBox);
+  return mainBox;
+}
+
+function CreateBox(appendTo, projectData)
+{
+  var box = document.createElement("DIV");
+  box.setAttribute("id", projectData["Abbreviation"]);
+  box.setAttribute("class", "Box");
+  var h3title = document.createElement("H3");
+  h3title.innerHTML = projectData["Title"];
+  box.appendChild(h3title);
+  CreateInformation(box, projectData);
+  appendTo.appendChild(box);
+}
+
+function CreateInformation(appendTo, projectData)
+{
+  var informationNode = document.createElement("P");
+  informationNode.innerHTML = "Genres:<br>";
+  for(var genre in projectData["Genres"])
+  {
+    informationNode.innerHTML += projectData["Genres"][genre] + "<br>";
+  }
+  informationNode.innerHTML += "<br>Description:<br>" + projectData["Description"];
+  appendTo.appendChild(informationNode);
+  return informationNode;
+}
+
+function CreateLinks(appendTo, projectData)
+{
+  for(var link in projectData["External Links"])
+  {
+    var aNode = document.createElement("A");
+    aNode.setAttribute("href", projectData["External Links"][link]);
+    aNode.innerHTML = link + "<br>";
+    appendTo.appendChild(aNode);
+    console.log(link);
+  }
+}
+
+function CreateButtons()
 {
   var elementArray = document.getElementsByClassName('Box');
   for (var i = 0; i < elementArray.length; i++) {
@@ -27,6 +151,13 @@ function StartupButtons()
     buttonNode.innerHTML = "Go to Project Page";
     elementArray[i].appendChild(buttonNode);
   }
+}
+
+function MainPageStartup()
+{
+  CreateCategories();
+  CreateSummaries();
+  CreateButtons();
 }
 
 function GetContactData()
@@ -47,19 +178,32 @@ function OpenProjectPage(ProjectName)
 {
   if (typeof(Storage) !== "undefined")
   {
-    localStorage.setItem("templateData", ProjectName);
-    window.open("content/main_page_template.html");
+    CommitDataToStorage("ProjectName", ProjectName);
+    window.open("content/projectPageTemplate.html");
   }
+}
+
+function CommitDataToStorage(Name, Data)
+{
+  if(typeof(Storage) !== "undefined")
+  {
+    localStorage.setItem(Name, Data);
+  }
+}
+
+function GetDataFromStorage(Name)
+{
+  if(typeof(Storage) !== "undefined")
+  {
+    return localStorage.getItem(Name);
+  }
+  return "NO_DATA";
 }
 
 function TemplateStartup()
 {
-  CurrentDataSet = JSON.parse(JSON.stringify(configData))["default"];
-  if (typeof(Storage) !== "undefined") {
-    var DataPointer = localStorage.getItem("templateData");
-    document.getElementById("Title").innerHTML = CurrentDataSet[DataPointer];
-  }
-  else {
-    console.log("Site did not load properly");
-  }
+  var DataItem = CurrentDataSet[GetDataFromStorage("ProjectName")];
+  document.getElementById("Title").innerHTML = DataItem["Title"];
+  CreateInformation(document.getElementById("Details"), DataItem);
+  CreateLinks(document.getElementById("Links"), DataItem);
 }
